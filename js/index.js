e83ebcd4f3e8e9595bf8505e30acc1caae17e4d5
Saikat Acharjee Joy <69462296+saikatcodec@users.noreply.github.com>
2021-07-19 17:53:02 +0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ let isUseEqual = false;
 // Calculator is reset or not
 let isReset = true;
 
+// Convert html collection to array
 btns = [...btns];
 
 // Add click event with button
@@ -24,90 +25,90 @@ btns.forEach(btn => {
 
 function btnClicked(btn) {
     /*
-    ** All logics gose here
-    * How button work
-    */
-    
+     ** All logics gose here
+     * How button work
+     */
+
     switch (btn.id) {
         case 'reset':
             display.value = 0;
             isReset = true;
             resetCal();
             break;
-            
+
         case 'plus-or-minus':
             negetivePositive(extractValue());
             break;
-        
+
         case 'percent':
             operator.push('%');
             workOfOpearator();
             break;
-            
+
         case 'sqrt':
             operator.push('√');
             workOfOpearator();
             break;
-            
+
         case 'seven':
             setDisplayValue(7);
             break;
-            
+
         case 'eight':
             setDisplayValue(8);
             break;
-        
+
         case 'nine':
             setDisplayValue(9);
             break;
-            
+
         case 'divide':
             operator.push('/');
             workOfOpearator();
             break;
-            
+
         case 'four':
             setDisplayValue(4);
             break;
-            
+
         case 'five':
             setDisplayValue(5);
             break;
-        
+
         case 'six':
             setDisplayValue(6);
             break;
-            
+
         case 'multi':
             operator.push('*');
             workOfOpearator();
             break;
-            
+
         case 'one':
             setDisplayValue(1);
             break;
-            
+
         case 'two':
             setDisplayValue(2);
             break;
-        
+
         case 'three':
             setDisplayValue(3);
             break;
-            
+
         case 'minus':
             operator.push('-');
             workOfOpearator();
             break;
-            
+
         case 'zero':
             setDisplayValue(0);
             break;
-            
+
         case 'dot':
             setDisplayValue('.');
             break;
-        
+
         case 'equal':
             operator.push('=');
             isUseEqual = true;
@@ -115,24 +116,24 @@ function btnClicked(btn) {
             // After doing sum calculator is reset
             resetCal();
             break;
-            
+
         case 'plus':
             operator.push('+');
             workOfOpearator();
             break;
-        
+
         default:
             console.log('Other');
     }
 }
 
 function negetivePositive(num) {
-    
+
     /*
-    ** Positive number convert to negetive number
-    ** Negetive number convert to positive number
-    */
-    
+     ** Positive number convert to negetive number
+     ** Negetive number convert to positive number
+     */
+
     if (num > 0) {
         num *= -1;
         display.value = num
@@ -146,23 +147,25 @@ function negetivePositive(num) {
 
 function extractValue() {
     // Extract Value from display
-    
+
     return display.value;
 }
 
 function setDisplayValue(value) {
     // Set Display value
-    
+
     // Take value from display
     let displayValue = extractValue();
-    
+
     // Check display value length
-    if ( displayValue.includes('-') ? displayValue.length < 9 : displayValue.length < 8 ) {
-        
+    if (displayValue.includes('-') ? displayValue.length < 9 : displayValue.length < 8) {
+
+        console.log(isUseOperator);
         // Check Operator are available or not
         if (isUseOperator || isUseEqual) {
             display.value = '';
             display.value += value;
+            console.log(isUseOperator);
             isUseOperator = false;
             isUseEqual = false;
         }
@@ -171,15 +174,12 @@ function setDisplayValue(value) {
             display.value = '';
             display.value += value;
             isReset = false;
-        }
-        else if (value.length < 8 && value !== '.') {
+        } else if (value.length < 8 && value !== '.') {
             display.value = value;
             isReset = true;
-            console.log(value);
-        }
-        else { // Otherwise add value of button
+        } else { // Otherwise add value of button
+            console.log('here');
             display.value += value;
-            console.log(value);
         }
     }
 }
@@ -191,38 +191,39 @@ function workOfOpearator() {
             case '+':
                 num1 += num2;
                 isUseOperator = true;
+                console.log(isUseOperator);
                 setDisplayValue(num1);
                 break;
-                
+
             case '-':
                 num1 -= num2;
                 isUseOperator = true;
                 setDisplayValue(num1);
                 break;
-                
+
             case '*':
                 num1 *= num2;
                 isUseOperator = true;
                 setDisplayValue(num1);
                 break;
             case '/':
-                num1 *= num2;
+                num1 /= num2;
                 isUseOperator = true;
                 setDisplayValue(num1);
                 break;
-                
+
             case '%':
                 num1 = num1 * (1 / 100);
                 isUseOperator = true;
                 setDisplayValue(num1);
                 break;
-                
+
             case '√':
                 num1 = Math.sqrt(num1);
                 isUseOperator = true;
                 setDisplayValue(num1);
                 break;
-            
+
             case '=':
                 workOfOpearator();
                 break;
@@ -238,4 +239,5 @@ function resetCal() {
     num2 = undefined;
     operator = [];
     isUseOperator = false;
+    isReset = true;
 }
